99f0ae687e430ebebc541bec5853e5293ed5e133
Hariii <harikrishnan.k.2024.ece@rajalakshmi.edu.in>
2026-03-26 22:58:17 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-  hotelName: "Krish hotel",
+  hotelName: "Dinesh hotel",
   supabaseUrl: "https://gtujlykdrfwqbdschsya.supabase.co",
   supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0dWpseWtkcmZ3cWJkc2Noc3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTc4MjMsImV4cCI6MjA4OTg5MzgyM30.Mahmwd7TwW9A20B3ksDqCBfOS5E4lqcBiZ2wRLQRlJk"
 };
