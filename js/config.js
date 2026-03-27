6dd8a7c1aa8bbf71308cb38ed5cde2dea266abc8
Hariii <harikrishnan.k.2024.ece@rajalakshmi.edu.in>
2026-03-27 10:42:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const APP_CONFIG = {
   supabaseUrl  : 'https://gtujlykdrfwqbdschsya.supabase.co',
   supabaseKey  : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0dWpseWtkcmZ3cWJkc2Noc3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTc4MjMsImV4cCI6MjA4OTg5MzgyM30.Mahmwd7TwW9A20B3ksDqCBfOS5E4lqcBiZ2wRLQRlJk',
   hotelName    : 'Dinesh ',
-  hotelsubtitle : 'Hotel'
+  hotelsubtitle : 'Hotel',
   primaryColor : '#E8621A',
   city         : 'chennai',
 };
